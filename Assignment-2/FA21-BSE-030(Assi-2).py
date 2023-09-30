6b7da089dd8a7856c4b63044227dd7bc785dca32
Muhammad Tayab <93065237+TayabGhafor@users.noreply.github.com>
2023-09-30 12:53:36 +0000

    FA21-BSE-030(Assi-2).py

--- Assignment-2/FA21-BSE-030(Assi-2).py
+++ Assignment-2/FA21-BSE-030(Assi-2).py
@@ -1,47 +1,36 @@
 import re
 
-# Reading the file
+# Read the file
 with open('example-text.txt', 'r') as file:
     text = file.read()
 
-# 1. Extracting list of all words.
-words = re.findall(r'\b\w+\b', text)
-
-# 2. Extracting list of all words starting with a capital letter.
-capital_words = re.findall(r'\b[A-Z]\w*\b', text)
-
-# 3. Extracting list of all words of length 5.
+# 1. Extract list of all words.
+all_words = re.findall(r'\b\w+\b', text)
+print("All Words:", all_words)
+# 2. Extract list of all words starting with a capital letter.
+capital_words = re.findall(r'\b[A-Z][a-z]*\b', text)
+print("Capital Words:", capital_words)
+# 3. Extract list of all words of length 5.
 five_letter_words = re.findall(r'\b\w{5}\b', text)
-
-# 4. Extracting list of all words inside double quotes.
-quoted_words = re.findall(r'"([^"]*)"', text)
-
-# 5. Extracting list of all vowels.
+print("Five-Letter Words:", five_letter_words)
+# 4. Extract list of all words inside double quotes.
+quoted_words = re.findall(r'"([^"]+)"', text)
+print("Quoted Words:", quoted_words)
+# 5. Extract list of all vowels.
 vowels = re.findall(r'[aeiouAEIOU]', text)
-
-# 6. Extracting list of 3 letter words ending with letter ‘e’.
+print("Vowels:", vowels)
+# 6. Extract list of 3 letter words ending with letter ‘e’.
 three_letter_e_words = re.findall(r'\b\w{3}e\b', text)
-
-# 7. Extracting list of all words starting and ending with letter ‘b’.
+print("Three-Letter Words Ending with 'e':", three_letter_e_words)
+# 7. Extract list of all words starting and ending with letter ‘b’.
 b_words = re.findall(r'\b[bB]\w*[bB]\b', text)
-
-# 8. Removing all punctuation marks from text.
+print("Words Starting and Ending with 'b':", b_words)
+# 8. Remove all the punctuation marks from the text.
 text_without_punctuation = re.sub(r'[^\w\s]', '', text)
-
-# 9. Replacing all words ending ‘n't’ to their full form ‘not’.
-text_replaced_not = re.sub(r'\b(\w+)n\'t\b', r'\1 not', text)
-
-# 10. Replacing all new lines with single-space.
+print("Text without Punctuation:", text_without_punctuation)
+# 9. Replace all words ending 'n't' to their full form 'not'.
+text_with_full_form = re.sub(r'\b(\w+)n\'t\b', r'\1 not', text)
+print("Text with Full Form of 'n't':", text_with_full_form)
+# 10. Replace all the new lines with a single space.
 text_single_space = re.sub(r'\n', ' ', text)
-
-# Printing the results
-print("1. All Words:", words)
-print("2. Capital Words:", capital_words)
-print("3. Five Letter Words:", five_letter_words)
-print("4. Quoted Words:", quoted_words)
-print("5. Vowels:", vowels)
-print("6. Three Letter Words Ending with 'e':", three_letter_e_words)
-print("7. Words Starting and Ending with 'b':", b_words)
-print("8. Text without Punctuation:\n", text_without_punctuation)
-print("9. Text with 'not' Replaced:", text_replaced_not)
-print("10. Text with New Lines Replaced by Spaces:\n", text_single_space)
+print("Text with New Lines Replaced:", text_single_space)
